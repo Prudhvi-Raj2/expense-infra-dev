e018caaf8b4b171bbac83fd7e0462d1ddfe4c070
prudh <prudhhviraj2@gmail.com>
2025-08-28 02:59:12 +0530

    terraform integration

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -7,4 +7,4 @@ dnf install ansible -y
 #ansible-playbook -i inventory mysql.yaml
 
 #pull
-ansible-pull -i localhost, -U "https://github.com/Prudhvi-Raj2/expense-ansible-roles-tf.git" main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
+ansible-pull -i localhost, -U https://github.com/Prudhvi-Raj2/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
