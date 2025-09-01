7fa021f51fee8fc90a135c96a67f85fa7ca9135c
prudh <prudhhviraj2@gmail.com>
2025-09-02 02:47:08 +0530

    front end is adding

--- 80-webalb/locals.tf
+++ 80-webalb/locals.tf
@@ -1,6 +1,8 @@
 locals {
   #stinglist to list
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-  app_alb_sg_id      = data.aws_ssm_parameter.app_alb_sg_id.value
+  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+  web_alb_sg_id     = data.aws_ssm_parameter.web_alb_sg_id.value
+  web_alb_certificate_arn = data.aws_ssm_parameter.web_alb_certificate_arn.value
+
 
 }
--- 80-webalb/main.tf
+++ 80-webalb/main.tf
@@ -1,33 +1,52 @@
 module "alb" {
   source   = "terraform-aws-modules/alb/aws"
-  internal = true
+  internal = false
 
   #expense-dev-app-alb
-  name                       = "${var.project_name}-${var.environment}-app-alb"
+  name                       = "${var.project_name}-${var.environment}-web-alb"
   vpc_id                     = data.aws_ssm_parameter.vpc_id.value
-  subnets                    = local.private_subnet_ids
+  subnets                    = local.public_subnet_ids
   create_security_group      = false
-  security_groups            = [local.app_alb_sg_id]
+  security_groups            = [local.web_alb_sg_id]
   enable_deletion_protection = false
   #security_groups = [local.app_alb_sg]
   tags = merge(
     var.common_tags,
     {
-      Name = "${var.project_name}-${var.environment}-app-alb"
+      Name = "${var.project_name}-${var.environment}-web-alb"
     }
   )
 
 }
 
-resource "aws_lb_listener" "frontend" {
+resource "aws_lb_listener" "https" {
     load_balancer_arn = module.alb.arn
     port              = "443"
     protocol          = "HTTPS"
     ssl_policy        = "ELBSecurityPolicy-2016-08"
-    certificate_arn   = data.aws_acm_certificate.app_alb_cert.arn
+    certificate_arn   = local.web_alb_certificate_arn
     
     default_action {
-        type             = "forward"
-        target_group_arn = var.target_group_arn
+        type             = "fixed-response"
+        
+        fixed_response {
+            content_type = "text/html"
+            message_body = "<h1> Hello, I am from the frontend ALB</h1>"
+            status_code  = "200"
+        }
     }
 }
+
+resource "aws_route53_record" "web_alb" {
+  zone_id = var.zone_id
+  name    = "*.${var.domain_name}"
+  type    = "A"
+
+  #these are ALB DNS names and zone id
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = false
+  }
+
+} 
